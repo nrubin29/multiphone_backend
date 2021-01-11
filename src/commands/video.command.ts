42ef50128dbf665b48ec3fe1ff99e3ff501b9fa6
nrubin29 <nrubin29@gmail.com>
2021-01-11 18:20:45 -0500

    Improved math for video command virtual canvas

--- src/commands/video.command.ts
+++ src/commands/video.command.ts
@@ -8,10 +8,18 @@ const videos: { [videoName: string]: { width: number; height: number } } = {
   },
 };
 
+// Dimensions of landscape iPhone 4(s) in pts
+const PHONE_WIDTH_PTS = 733.5;
+const PHONE_HEIGHT_PTS = 376.53;
+
+// Bezels of an iPhone 4(s) in pts
+const TOP_BEZEL_PTS = 126.75;
+const SIDE_BEZEL_PTS = 28.265;
+
 export default class VideoCommand extends Command {
   async run(args: string[]) {
-    if (args.length === 0 || !(args[0] in videos)) {
-      console.log("Unknown video");
+    if (args.length < 2 || !(args[0] in videos)) {
+      console.log("Usage: video <video name> <grid width in # devices>");
       return;
     }
 
@@ -19,39 +27,31 @@ export default class VideoCommand extends Command {
     await sleep(1500);
 
     const video = videos[args[0]];
+    const gridWidth = parseInt(args[1]);
 
-    // Hardcoding a 2x2 grid
-    const gridWidth = 2;
-    const gridHeight = 2;
-
-    // Dimensions of landscape iPhone 4(s) in pts
-    const phoneWidthPts = 733.5;
-    const phoneHeightPts = 376.53;
-
-    const virtualCanvasWidth = phoneWidthPts * gridWidth;
-    const virtualCanvasHeight = phoneHeightPts * gridHeight;
+    const virtualCanvasWidth = PHONE_WIDTH_PTS * gridWidth;
 
     // Scale the video such that the width is equal to virtualCanvasWidth and
     // the height preserves the aspect ratio.
     const scaledVideoHeight = video.height * (virtualCanvasWidth / video.width);
 
-    let i = 0;
-    for (let col = 0; col < gridWidth; col++) {
-      for (let row = 0; row < gridHeight; row++) {
-        const socket = this.socketManager.getSocket(i++);
+    await this.socketManager.forEachAsync((socket, i) => {
+      const row = Math.floor(i / gridWidth);
+      const col = i % gridWidth;
 
-        // TEMP
-        if (!socket) {
-          continue;
-        }
+      const screenTopLeftX = PHONE_WIDTH_PTS * row + TOP_BEZEL_PTS;
+      const screenTopLeftY = PHONE_HEIGHT_PTS * col + SIDE_BEZEL_PTS;
 
-        socket.send(
-          `${args[0]} ${virtualCanvasWidth} ${scaledVideoHeight} ${
-            phoneWidthPts * row
-          } ${phoneHeightPts * col}`
-        );
-      }
-    }
+      socket.send(
+        [
+          args[0],
+          virtualCanvasWidth,
+          scaledVideoHeight,
+          screenTopLeftX,
+          screenTopLeftY,
+        ].join(" ")
+      );
+    });
 
     await sleep(3000);
     await this.socketManager.emitToAll("play");
